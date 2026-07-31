348d99dacfe82530b168bbca915c6f6b438c1098
Nguyễn Minh Quân <quannguyenkm16122004@gmail.com>
2026-07-31 23:31:57 +0700

    feat(ui): update version numbers and enhance UX for customer and supplier views with concise messaging

--- dlm-erp/dl_technical/__manifest__.py
+++ dlm-erp/dl_technical/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "DL-Technical",
-    "version": "17.0.1.9.0",
+    "version": "17.0.1.10.0",
     "summary": "Quản lý BOM, Bản vẽ, Bán thành phẩm, công thức",
     "author": "Dai Linh",
     "category": "Hidden",
